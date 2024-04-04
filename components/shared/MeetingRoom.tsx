3445380fef6568d23aaca1270ad2ebbbd298cbdc
uttamsutariya <uttam.sutariya@capitsign.com>
2024-04-04 15:37:59 +0530

    added end call for everyone

--- components/shared/MeetingRoom.tsx
+++ components/shared/MeetingRoom.tsx
@@ -1,15 +1,28 @@
 import { useState } from "react";
-import { CallControls, CallParticipantsList, CallStatsButton, PaginatedGridLayout, SpeakerLayout } from "@stream-io/video-react-sdk";
+import { CallControls, CallParticipantsList, CallStatsButton, CallingState, PaginatedGridLayout, SpeakerLayout, useCallStateHooks } from "@stream-io/video-react-sdk";
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
 import { cn } from "@/lib/utils";
-import { LayoutList } from "lucide-react";
+import { LayoutList, Users } from "lucide-react";
 import _ from "lodash";
+import { useSearchParams } from "next/navigation";
+import EndCallButton from "./EndCallButton";
+import Loader from "./Loader";
 
 type CallLayoutType = "grid" | "speaker-left" | "speaker-right";
 
 const MeetingRoom = () => {
+    const searchParams = useSearchParams();
+    const isPersonalRoom = !!searchParams.get("personal");
+
     const [layout, setLayout] = useState<CallLayoutType>("speaker-left");
     const [showParticipents, setShowParticipents] = useState(false);
+
+    const { useCallCallingState } = useCallStateHooks();
+    const callingState = useCallCallingState();
+
+    if (callingState !== CallingState.JOINED) {
+        return <Loader />;
+    }
 
     const CallLayout = () => {
         switch (layout) {
@@ -38,7 +51,7 @@ const MeetingRoom = () => {
                     <CallParticipantsList onClose={() => setShowParticipents(false)} />
                 </div>
             </div>
-            <div className="fixed bottom-0 flex w-full items-center justify-center gap-5">
+            <div className="fixed bottom-0 flex w-full items-center justify-center gap-5 flex-wrap">
                 <CallControls />
                 <DropdownMenu>
                     <div className="flex items-center">
@@ -65,6 +78,16 @@ const MeetingRoom = () => {
                     </DropdownMenuContent>
                 </DropdownMenu>
                 <CallStatsButton />
+                <button
+                    onClick={() => {
+                        setShowParticipents((prev) => !prev);
+                    }}
+                >
+                    <div className="cursor-pointer rounded-2xl bg-[#19232d] px-4 py-2 hover:bg-[#4c535b]">
+                        <Users size={20} className="text-white" />
+                    </div>
+                </button>
+                {!isPersonalRoom && <EndCallButton />}
             </div>
         </section>
     );
